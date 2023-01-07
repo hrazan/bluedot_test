42a28760f58292b10e1a685520dec65ff9988fda
hrazan <hafiyanda.razan@gmail.com>
2023-01-07 19:54:25 +0900

    added d-pad

--- bluedot_test.py
+++ bluedot_test.py
@@ -1,5 +1,26 @@
 from bluedot import BlueDot
-bd = BlueDot()
-bd.wait_for_press()
-print("PRESSED!")
+from signal import pause
 
+def front(pos):
+    print("front")
+    
+def back(pos):
+    print("back")
+    
+def left(pos):
+    print("left")
+    
+def right(pos):
+    print("right")
+
+bd = BlueDot(cols=3, rows=3)
+
+bd[0,0].visible = False
+bd[1,0].when_pressed = front
+bd[2,0].visible = False
+bd[0,1].when_pressed = left
+bd[1,1].visible = False
+bd[2,1].when_pressed = right
+bd[0,2].visible = False
+bd[1,2].when_pressed = back
+bd[2,2].visible = False
